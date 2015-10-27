8ffc1107114702da896f166765b47e4a106c1579
philmphoenix <jpholzmann@gmail.com>
2015-10-26 20:40:58 -0600

    Adds user model and login function
    Now a user is set up with the basic features.

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  def self.banner_converter(auth_info)
+    auth_info.extra.raw_info.profile_banner_url + "/600x200"
+  end
+
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |new_user|
+      new_user.uid                = auth_info.uid
+      new_user.name               = auth_info.extra.raw_info.name
+      new_user.screen_name        = auth_info.extra.raw_info.screen_name
+      new_user.uid                = auth_info.extra.raw_info.user_id
+      new_user.banner_image       = User.banner_converter(auth_info)
+      new_user.profile_image      = auth_info.info.image
+      new_user.oauth_token        = auth_info.credentials.token
+      new_user.oauth_token_secret = auth_info.credentials.secret
+    end
+  end
+end
