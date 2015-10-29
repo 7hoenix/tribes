9648ac9257b85ba92af6d1fa69b9dece8a71f4b3
philmphoenix <jpholzmann@gmail.com>
2015-10-29 00:40:25 -0600

    Adds the ugliest test I've ever written for retweet
    Retweet feature test is functioning. But good lord is it gross.

--- spec/features/user_can_retweet_a_tweet_spec.rb
+++ spec/features/user_can_retweet_a_tweet_spec.rb
@@ -6,10 +6,18 @@ RSpec.describe "user can retweet a tweet", type: :feature, vcr: true do
       visit "/"
       click_on "Login"
 
-      VCR.use_cassette("/user_can_retweet_a_tweet after") do
-        within(css: "retweeting-1") do
+      current_user = User.last
+      VCR.use_cassette("/user_can_retweet_a_tweet during") do
+        tweets = UserPresenter.new(current_user).tweets
+        @tweet = tweets.find { |tweet| tweet.id == 659215168258183168 }
+        expect(@tweet.retweet_count).to eq(0)
+      end
+      within(".retweet-#{@tweet.id}") do
           click_on "Retweet"
-          expect(retweet_count).to eq(1)
+        VCR.use_cassette("/user_can_retweet_a_tweet after") do
+          tweets = UserPresenter.new(current_user).tweets
+          new_tweet = tweets.find { |tweet| tweet.id == 659215168258183168 }
+          expect(new_tweet.retweet_count).to eq(1)
         end
       end
     end
