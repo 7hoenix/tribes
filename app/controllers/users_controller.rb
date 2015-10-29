bb607106c70c17bf0998e7f97222360e28ad8048
philmphoenix <jpholzmann@gmail.com>
2015-10-29 11:25:06 -0600

    Adds unfollow action to users controller
    This route doesn't follow the typical restful pattern
    but I opted to use it anyways as a unfollow controller
    /resource just didn't feel right to me.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = UserPresenter.new(current_user)
   end
+
+  def unfollow
+    current_user.service.unfollow(params[:screen_name])
+    redirect_to current_user
+  end
 end
